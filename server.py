274fa51bafbd22a64c7bf022b05c9d12bb9bf421
j341nono <kanakana.nono1224@gmail.com>
2025-07-22 22:34:07 +0900

    change port exec using env

--- server.py
+++ server.py
@@ -46,8 +46,12 @@ def home():
     #     return make_response(jsonify(response_data), 401) # 401 Unauthorized
 
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5001)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    import os
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
 
 
 @app.route('/api/login', methods=['POST'])
